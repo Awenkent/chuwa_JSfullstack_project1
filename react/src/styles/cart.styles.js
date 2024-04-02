359e75529761d9868e0fd328bd4f4da332e53db0
Weizhou Wen <58341131+WeizhouWen@users.noreply.github.com>
2024-04-01 20:03:14 -0700

    Update cart.styles.js

--- react/src/styles/cart.styles.js
+++ react/src/styles/cart.styles.js
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
-  width: 300px;
+  width: 500px;
   padding: 20px;
+  @media only screen and (max-width: 600px) {
+    width: 90%; /* Change width to 100% for screens smaller than 600px */
+  }
 `;
+
+
 
 export { Wrapper };
