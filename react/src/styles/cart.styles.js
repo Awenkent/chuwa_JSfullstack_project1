c8933a94dc9d1c24766c8cb93b0aff76fedc5948
Weizhou Wen <58341131+WeizhouWen@users.noreply.github.com>
2024-04-01 20:07:43 -0700

    Revert "Update cart.styles.js"
    This reverts commit 359e75529761d9868e0fd328bd4f4da332e53db0.

--- react/src/styles/cart.styles.js
+++ react/src/styles/cart.styles.js
@@ -2,13 +2,8 @@ import styled from "styled-components";
 
 const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
-  width: 500px;
+  width: 300px;
   padding: 20px;
-  @media only screen and (max-width: 600px) {
-    width: 90%; /* Change width to 100% for screens smaller than 600px */
-  }
 `;
-
-
 
 export { Wrapper };
